9a3a2baa9e75e05e091b5e40f36aa93e8bc56331
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-01-06 18:06:08 +0500

    feature: Offset model class

--- models.py
+++ models.py
@@ -7,3 +7,14 @@ class Position:
     def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
+
+
+class Offset:
+    """Offset from the position in pixels"""
+
+    vertical: float
+    horizontal: float
+
+    def __init__(self, vertical: float, horizontal: float) -> None:
+        self.vertical = vertical
+        self.horizontal = horizontal
