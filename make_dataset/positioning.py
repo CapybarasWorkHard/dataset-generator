af3b2dabf6244dd97289b0452b187bd030fb194b
SolovovChann <solnik2001@gmail.com>
2023-04-18 12:55:58 +0500

    chore: transform Point into the data class

--- make_dataset/positioning.py
+++ make_dataset/positioning.py
@@ -1,22 +1,17 @@
+from dataclasses import dataclass
 import random
 
 
+@dataclass
 class Point:
     """Position of anything on the image with pixels"""
 
     x: int
     y: int
 
-    def __init__(self, x: int, y: int) -> None:
-        self.x = x
-        self.y = y
-
     def __iter__(self):
         for attr in self.x, self.y:
             yield attr
-
-    def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.x}, {self.y})'
 
     def shift(self, vertical: int, horizontal: int) -> 'Point':
         """Get new shifted position"""
