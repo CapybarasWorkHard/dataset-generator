766fc7f63f512a251d1c7dfb962d4feb91469f71
SolovovChann <solnik2001@gmail.com>
2023-04-18 19:00:31 +0500

    feat: get_field_multiline_text_position util

--- make_dataset/utils.py
+++ make_dataset/utils.py
@@ -10,3 +10,22 @@ def get_field_text_position(font: Font, field: Field) -> tuple[int, ...]:
         a + b
         for a, b in zip(center * 2, bbox)
     )
+
+
+def get_field_multiline_text_position(
+    font: Font,
+    field: Field,
+) -> list[tuple[int, ...]]:
+    positions: list[tuple[int, ...]] = []
+
+    for index, line in enumerate(field.value.splitlines()):
+        bbox = font.pil_font.getbbox(line, anchor=font.anchor)
+        offset = int(index * (font.size + font.spacing))
+        new_center = tuple(field.point.shift(0, offset))
+        text_pos = tuple(
+            a + b
+            for a, b in zip(new_center * 2, bbox)
+        )
+        positions.append(text_pos)
+
+    return positions
